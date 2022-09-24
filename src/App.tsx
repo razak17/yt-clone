75157bb0576467a7449678ea0124d8836cc36378
Razak <moabdulrazak17@gmail.com>
2022-09-24 02:42:14 +0000

    chore(routes): configure react-router-dom
    create template for all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { Box } from '@mui/material';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ChannelDetail from './components/ChannelDetail';
+import Feed from './components/Feed';
+import Navbar from './components/Navbar';
+import SearchFeed from './components/SearchFeed';
+import VideoDetail from './components/VideoDetail';
+
 const App = () => {
 	return (
-		<div>
-			<h1>Hello Mom!</h1>
-		</div>
+		<BrowserRouter>
+			<Box sx={{ backgroundColor: '#000' }}>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Feed />} />
+					<Route path='/video/:id' element={<VideoDetail />} />
+					<Route path='/channel/:id' element={<ChannelDetail />} />
+					<Route path='/search/:searchTerm' element={<SearchFeed />} />
+				</Routes>
+			</Box>
+		</BrowserRouter>
 	);
 };
 
