de59c560b7b9991adcf53a80e5c8bdca4b788ba8
Razak <moabdulrazak17@gmail.com>
2022-09-24 06:00:00 +0000

    feat(pages): channel details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Box } from '@mui/material';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ChannelDetail from './components/ChannelDetail';
+
 import Feed from './components/Feed';
 import Navbar from './components/Navbar';
 import SearchFeed from './components/SearchFeed';
 import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
 
 const App = () => {
 	return (
--- src/components/ChannelDetail.tsx
+++ src/components/ChannelDetail.tsx
@@ -1,5 +1,41 @@
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { useQuery } from 'react-query';
+import { getVideos } from '../lib/api';
+import ChannelCard from './ChannelCard';
+import Videos from './Videos';
+import { Videos as VideosType, Video } from '../types';
+
 const ChannelDetail = () => {
-	return <div>ChannelDetail</div>;
+	const { id } = useParams();
+
+	const { data: channelInfo } = useQuery(['channelDetail', id], () =>
+		getVideos(`channels?part=snippet&id=${id}`)
+	);
+
+	const { data: videos, isLoading: videosLoading } = useQuery(['channelVideos', id], () =>
+		getVideos(`search?channelId=${id}&part=snippet%2Cid&order=date`)
+	);
+
+	return (
+		<Box minHeight='95vh'>
+			<Box>
+				<div
+					style={{
+						height: '300px',
+						background:
+							'linear-gradient(90deg, rgba(0,238,247,1) 0%, rgba(206,3,184,1) 100%, rgba(0,212,255,1) 100%)',
+						zIndex: 10
+					}}
+				/>
+				<ChannelCard channelDetail={channelInfo?.items[0] as Video} marginTop='-120px' />
+			</Box>
+			<Box p={2} display='flex'>
+				<Box sx={{ mr: { sm: '100px' } }} />
+				<Videos isLoading={videosLoading} videos={videos as VideosType} />
+			</Box>
+		</Box>
+	);
 };
 
 export default ChannelDetail;
