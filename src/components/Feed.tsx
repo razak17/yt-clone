fa9a24429c4335fbfc98adcb1aa4c341bb855c2b
Razak <moabdulrazak17@gmail.com>
2022-09-24 03:44:07 +0000

    feat(components): create feed

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import Videos from './Videos';
+import Sidebar from './Sidebar';
+
 const Feed = () => {
-	return <div>Feed</div>;
+	const [selectedCategory, setSelectedCategory] = useState('New');
+
+	return (
+		<Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+			<Box
+				sx={{
+					height: { sx: 'auto', md: '92vh' },
+					borderRight: '1px solid #3d3d3d',
+					px: { sx: 0, md: 2 }
+				}}
+			>
+				<Sidebar />
+
+				<Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
+					Copyright © 2022 YouTube
+				</Typography>
+			</Box>
+
+			<Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+				<Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: 'white' }}>
+					{selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
+				</Typography>
+
+				<Videos />
+			</Box>
+		</Stack>
+	);
 };
 
 export default Feed;
